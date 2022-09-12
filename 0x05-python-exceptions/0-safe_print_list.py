2b37727114da7dc781fd46acd945711dd78ba032
Robel Pop <robelpopandro@gmail.com>
2022-09-12 13:38:10 +0300

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-	for i in range(x):
-		try:
-			print('{:d}'.format(my_list[i]),end='')
-		except IndexError:
-			x-=1
-	print()
-	return x
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]), end='')
+        except IndexError:
+            x -= 1
+    print()
+    return x
