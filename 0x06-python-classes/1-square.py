2e05534183a96ec2ce78837a5b8f114f648c32b8
Robel Pop <robelpopandro@gmail.com>
2022-09-14 09:05:37 +0300

    Task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+empty square classes that defines a square.
+'''
+
+
+class Square:
+    '''
+        Square defines a sqaures
+    '''
+    def __init__(self, size):
+        '''
+        Args:
+            size (int): positive size of the square
+        '''
+        self.__size = size
