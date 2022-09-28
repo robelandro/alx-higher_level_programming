008739cb29f1c9fb664d79a36cab86ba5ae5a311
Robel Pop <robelpopandro@gmail.com>
2022-09-29 00:13:27 +0300

    fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
-"""
-A class Student that defines a student by public instance attributes
+"""Module 11-student.
+Creates a Student class.
 """
 
 
 class Student:
-    """ A class Student that defines a student by public instance attributes
+    """Class that defines a student.
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method to_json().
     """
+
     def __init__(self, first_name, last_name, age):
+        """Initializes the Student instance."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ A function that returns the dictionary description with simple
+        """Retrieves a dictionary representation
+        of a Student instance.
 
-        Returns:
-            [dict]: [attributes of the given instance]
+        Returns: the dict representation of the instance.
         """
-        return vars(Student)
+
+        return self.__dict__
