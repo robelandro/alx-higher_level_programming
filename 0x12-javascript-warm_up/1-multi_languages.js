bd45ffe042eeb7ced54e0c77cb1273599785b31c
Robel Pop <robelpopandro@gmail.com>
2022-11-29 00:19:20 +0300

    frist and seconed task done

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const programing = {
+  c_code: 'C is fun',
+  py_code: 'Python is cool',
+  js_code: 'javaScript is amazing'
+};
+
+console.log(programing.c_code);
+console.log(programing.py_code);
+console.log(programing.js_code);
