29fb23390dd0f139269a3de8ddff7381be9eae52
Robel Pop <robelpopandro@gmail.com>
2022-11-29 09:56:55 +0300

    seconed task done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
 
+const value = process.argv
 
+if (value.length === 3){
+	console.log('Argument found')
+}
+else if (value.length > 3){
+	console.log('Arguments found')
+}
+else{
+	console.log('No argument”')
+}
