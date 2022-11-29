d42b43285a109be56c2e68c135378aed05000d1e
Robel Pop <robelpopandro@gmail.com>
2022-11-29 10:00:50 +0300

    fixed semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-const value = process.argv
+const value = process.argv;
 
-if (value.length === 3){
-	console.log('Argument found')
-}
-else if (value.length > 3){
-	console.log('Arguments found')
-}
-else{
-	console.log('No argument”')
+if (value.length === 3) {
+  console.log('Argument found');
+} else if (value.length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
