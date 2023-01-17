3a03bdd054a82f9397ecf65fe41d1f3174f0a2c5
Robel Pop <robelpopandro@gmail.com>
2023-01-17 23:56:24 +0300

    Done 1

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Write a script that prints all characters of a Star Wars movie:
+const request = require('request');
+const { argv } = require('process');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
+request(url, function (err, res) {
+  if (err) console.log(err);
+  else {
+    const film = JSON.parse(res.body);
+    for (const characterUrl of film.characters) {
+      request(characterUrl, function (err, res) {
+        if (err) console.log(err);
+        else {
+          const character = JSON.parse(res.body);
+          console.log(character.name);
+        }
+      });
+    }
+  }
+});
