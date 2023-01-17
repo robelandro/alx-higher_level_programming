bcbebe7d1b91ea97a8ec764b4b202b379e0b1bec
Robel Pop <robelpopandro@gmail.com>
2023-01-17 23:56:30 +0300

    Done 7

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Write a script that gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  try {
+    fs.writeFile(process.argv[3], body, 'utf8', function (err, result) { if (err) console.log(err); });
+  } catch (err) {
+    console.log(err);
+  }
+});
